7387161c1d6eb6703d7f8a0db9113cf7caf85116
Duttji1996 <96298162+Duttji1996@users.noreply.github.com>
2022-04-10 20:21:24 +0530

    Update route.js
    issue was in pushing. this is why i updated it with exact assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,58 +3,73 @@ const res = require('express/lib/response');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api, class will be start today, its 8th april ')
+//assignment problem 1
+
+router.get('/movieList', function (req, res) {
+   let movieArr= ["Rang De Basanti","Maa Sarswati","Maa Vaishno","Maa Vindhyawasini","Function UP"]
+
+    res.send( movieArr )
 });
 
-    // print first 9 natural number sum.
-    router.get('/missingArr', function (req, res) {
-        let b=[1,2,3,4,5,7,8,9]
-        let missing_number ="";
-        let sum=0;
-        let sumb=0;
-        for(let i=1;i<10;i++){
-            sum= sum + i;
+//assignment problem 2 & 3
+
+router.get('/movies/:indexNumber', function (req, res) {
+    let movieAr= ["Rang De Basanti","Maa Sarswati","Maa Vaishno","Maa Vindhyawasini","Function UP"]
+    let index=req.params.indexNumber;
+            if(index>movieAr.length-1){
+                res.send("no index number match")
+                return;
+            }
+            res.send(movieAr[index]) 
+ });
+
+ //assignment problem 4
+
+ router.get('/films', function (req, res) {
+    let movieArr= [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       
+ 
+     res.send( movieArr )
+ });
+
+ //assignment problem 5
+
+ router.get('/films/:id', function (req, res) {
+     let index= req.params.id;
+    let movieArr= [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+    for(let i=0; i<movieArr.length;i++){
+        if(movieArr[i].id==index){
+            res.send(movieArr[i])
+            break;
         }
-       for(let j=0; j<b.length;j++){
-        sumb=sumb + b[j];
-       }
-       missing_number=missing_number + (sum-sumb);
+    }
+ 
+     res.send( "no id match with any films" )
+ });
 
-       res.send(missing_number);
-    });
-
-    router.get('/Missed', function (req, res) {
-        let p=[33,34,35,37,38]
-        let missing="";
-        let SumN= 0;
-        let sum=0;
-        for(let n=33; n<39;n++){
-            SumN=SumN+n;
-        }
-        for(let m=0;m<p.length;m++){
-            sum=sum+p[m];
-        }
-        missing=missing+(SumN-sum)
-        let arr =[];
-        arr.push(missing)
-        res.send(arr)
-        
-     
-    });
-    router.post('/ArrPost', function (req, res) {
-        let Body= req.body.likes
-        let arr=['Maa Saraswati','Maa Vaishno','Maa Kali','Maa Vindhyawasini','MyFunctionUp']
-        arr.push(Body)
-           
-           res.send(arr)
-    });
-
-
-    
-    module.exports = router;
+module.exports = router;
 // // adding this comment for no reason
