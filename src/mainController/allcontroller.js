e52d33fb28d4e989936934cd31ed6c2766106f1e
Duttji1996 <prakashdutttripathi@gmail.com>
2022-04-26 04:26:20 +0530

    new assignment adhura h

---
+++ src/mainController/allcontroller.js
@@ -0,0 +1,117 @@
+let axios=require("axios")
+
+let getState= async function(req,res){
+    let website= {
+        method: "get",
+        url:'https://cdn-api.co-vin.in/api/v2/admin/location/states'
+    }
+
+    let fetchdata= await axios(website)
+
+    console.log("fetch data:  ",fetchdata.data)
+    res.send(fetchdata.data)
+}
+
+//for practise
+
+let getDistricts= async function(req,res){
+    let id =req.params.stateid
+    let website={
+        method:"get",
+        url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
+    }
+
+    let fetch= await axios(website)
+
+    console.log("fetch line numnber 23:  ", fetch.data)
+
+    res.send(fetch.data)
+}
+
+// // 1st Problem ==========================================================================================
+
+let appointment= async function(req,res){
+    let query= req.query
+    console.log("query find:  ",query)
+
+    let website={
+        method: "get",
+        url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${query.district_id}&date=${query.date}`
+    }
+
+    let fetch =await axios(website)
+
+    console.log("all detail:  ",fetch.data)
+    
+
+    res.send(fetch.data)
+
+}
+
+//Practise=============================================================================================
+
+let getOtp= async function(req,res){
+    let body=req.body
+
+    let website= {
+        method: 'post',
+        url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
+        data: body
+    }
+
+    let fetch = await axios(website)
+
+    console.log("thik h yha tak:  ", fetch.data)
+
+    res.send(fetch.data)
+}
+
+//============================================================================================================
+
+let weather= async function(req,res){
+    let query=req.query;
+
+    let website= {
+        method:"get",
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${query.q}&appid=${query.appid}`
+    }
+
+    let fetch = await axios(website)
+
+    console.log("fetch:  ",fetch.data.name)
+    let city= fetch.data.name
+
+    let temperature= fetch.data.main.temp
+
+    let result =parseFloat(temperature)
+
+    console.log("result:   ",result);
+
+    res.send({name:city, temp: result});
+}
+
+// ===============================================================================================================
+//3rd Problem
+
+let meme= async function(req,res){
+    let query=req.query
+
+    let website= {
+        method: "post",
+        url:`https://api.imgflip.com/caption_image?template_id=${query.template_id}&text0=${query.text0}&text1=${query.text1}&username=${query.username}&password=${query.password}`
+    }
+
+    let result= await axios(website)
+
+    console.log(result.data)
+
+   res.send(result.data)
+}
+
+
+module.exports.weather=weather
+module.exports.getOtp=getOtp
+module.exports.getState=getState
+module.exports.getDistricts=getDistricts
+module.exports.appointment=appointment
+module.exports.meme=meme
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
+const newController= require("../mainController/allcontroller")
 
 
 
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/cowin/states", CowinController.getStates)
+ router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+// router.get("/cowin/getByPin", CowinController.getByPin)
 
-
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-
-router.post("/cowin/getOtp", CowinController.getOtp)
+// router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
+router.get("/getState",newController.getState)                  // Practise
 
+router.get("/getDistricts/:stateid",newController.getDistricts)
+
+router.get("/appointment",newController.appointment)                // 1st problem API
+
+router.post("/getOtp", newController.getOtp)                        // Practise
+
+router.get("/weather",newController.weather)                        // 2nd Problem
+
+router.post("/meme",newController.meme)                             // 3rd Problem API
 
 
 module.exports = router;
