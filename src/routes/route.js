b394840cbfee575ff443d5dd5d90d26e109aca03
Duttji1996 <prakashdutttripathi@gmail.com>
2022-04-26 00:05:44 +0530

    new assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const CowinController= require("../controllers/cowinController")
 
 
 
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -16,6 +17,8 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+
+
 
 
 
